c3b031f8dce157a2c4a5569e81654b09c85a5f71
Lasitha Weligampola <lasiprabo@gmail.com>
2023-09-03 13:05:32 -0400

    repository

---
+++ app/src/main/java/com/lasithaprabodha/noteapp/feature_note/domain/repository/NoteRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.lasithaprabodha.noteapp.feature_note.domain.repository
+
+import com.lasithaprabodha.noteapp.feature_note.data.data_source.NoteDao
+import com.lasithaprabodha.noteapp.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepositoryImpl(
+    private val dao: NoteDao
+) :NoteRepository {
+    override fun getNotes(): Flow<List<Note>> {
+        return dao.getNotes()
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+       return dao.getNoteById(id)
+    }
+
+    override suspend fun insertNote(note: Note) {
+        dao.insertNote(note)
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        dao.deleteNote(note)
+    }
+}
